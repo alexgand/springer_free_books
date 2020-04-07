9024647dbf7abba8791467b0878e7af208f20f2f
viidix <54764036+viidix@users.noreply.github.com>
2020-04-07 08:17:16 +0200

    argparse implementation
    Download folder and excel file can now be passed as optional arguments

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import os
 import requests
 import pandas as pd
 from tqdm import tqdm
+import argparse
 
-# insert here the folder you want the books to be downloaded:
-folder = os.getcwd() + '/download/'
+parser = argparse.ArgumentParser(description="Python script to download all Springer books released for free during the 2020 COVID-19 quarantine")
+parser.add_argument("-d", "--download-folder", type=str, action="store", dest="folder", default=os.getcwd() + '/download/', help="folder you want the books to be downloaded")
+parser.add_argument("--excel-file", type=str, action="store", dest="books", default='https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4', help="url or path to the excel file")
+
+args = parser.parse_args()
+
+folder = args.folder
 
 if not os.path.exists(folder):
     os.mkdir(folder)
 
-books = pd.read_excel('https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4')
+books = pd.read_excel(args.books)
 
 # save table:
 books.to_excel(folder + 'table.xlsx')
