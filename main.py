a96d741e28d6986e2b542de457edb0248544614c
chaosAD <frasker@hotmail.com>
2020-04-25 06:11:08 +0800

    Did code cleanup and added bookname generator
    Removed all code duplicate and added compose_bookname()
    to generate better bookname. Also, this new code prevents
    incomplete book, due to Ctrl+C and Ctrl+Break, to be
    accepted as valid book.

--- main.py
+++ main.py
@@ -2,76 +2,39 @@
 
 import os
 import requests
-import shutil
 import pandas as pd
 from tqdm import tqdm
+from helper import *
 
-# insert here the folder you want the books to be downloaded:
-folder = os.path.join(os.getcwd(), 'downloads')
+folder = create_relative_path_if_not_exist('downloads')
 
-if not os.path.exists(folder):
-    os.mkdir(folder)
-
-if not os.path.exists(os.path.join(folder, "table.xlsx")):
-    books = pd.read_excel('https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4')
-
-    # save table:
-    books.to_excel(os.path.join(folder, 'table.xlsx'))
+table_url = 'https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4'
+table = 'table_' + table_url.split('/')[-1] + '.xlsx'
+if not os.path.exists(os.path.join(folder, table)):
+    books = pd.read_excel(table_url)
+    # Save table
+    books.to_excel(os.path.join(folder, table))
 else:
-    books = pd.read_excel(os.path.join(folder, 'table.xlsx'), index_col=0, header=0)
-
-print('Download started.')
+    books = pd.read_excel(os.path.join(folder, table), index_col=0, header=0)
 
 
-for url, title, author, pk_name in tqdm(books[['OpenURL', 'Book Title', 'Author', 'English Package Name']].values):
-
-    new_folder = os.path.join(folder, pk_name)
+for url, title, author, edition, isbn, category in tqdm(books[['OpenURL', 'Book Title', 'Author', 'Edition', 'Electronic ISBN', 'English Package Name']].values):
+    new_folder = os.path.join(folder, category)
 
     if not os.path.exists(new_folder):
         os.mkdir(new_folder)
 
     r = requests.get(url)
-    new_url = r.url
+    new_url = r.url.replace('%2F','/').replace('/book/','/content/pdf/') + '.pdf'
+    bookname = compose_bookname(title, author, edition, isbn)
+    output_file = os.path.join(new_folder, bookname + '.pdf')
+    download_book(new_url, output_file)
 
-    new_url = new_url.replace('/book/','/content/pdf/')
+    # Download EPUB version too if exists
+    new_url = r.url.replace('%2F','/').replace('/book/','/download/epub/') + '.epub'
+    output_file = os.path.join(new_folder, bookname + '.epub')
+    request = requests.get(new_url, stream = True)
+    if request.status_code == 200:
+       download_book(new_url, output_file)
 
-    new_url = new_url.replace('%2F','/')
-    new_url = new_url + '.pdf'
-
-    final = new_url.split('/')[-1]
-    final = title.replace(',','-').replace('.','').replace('/',' ').replace(':',' ') + ' - ' + author.replace(',','-').replace('.','').replace('/',' ').replace(':',' ') + ' - ' + final
-    final = final.encode('ascii', 'ignore').decode('ascii')
-    final = (final[:145] + '.pdf') if len(final) > 145 else final
-    output_file = os.path.join(new_folder, final)
-
-    if not os.path.exists(output_file.encode('utf-8')):
-        with requests.get(new_url, stream=True) as req:
-            try:
-                with open(output_file.encode('utf-8'), 'wb') as out_file:
-                    shutil.copyfileobj(req.raw, out_file)
-            except OSError:
-                print("Error: PDF filename appears incorrect.")
-
-        #download epub version too if exists
-        new_url = r.url
-
-        new_url = new_url.replace('/book/','/download/epub/')
-        new_url = new_url.replace('%2F','/')
-        new_url = new_url + '.epub'
-
-        final = new_url.split('/')[-1]
-        final = title.replace(',','-').replace('.','').replace('/',' ').replace(':',' ') + ' - ' + author.replace(',','-').replace('.','').replace('/',' ').replace(':',' ') + ' - ' + final
-        final = final.encode('ascii', 'ignore').decode('ascii')
-        final = (final[:145] + '.epub') if len(final) > 145 else final
-        output_file = os.path.join(new_folder, final)
-
-        request = requests.get(new_url)
-        if request.status_code == 200:
-            with requests.get(new_url, stream=True) as req:
-                try:
-                    with open(output_file.encode('utf-8'), 'wb') as out_file:
-                        shutil.copyfileobj(req.raw, out_file)
-                except OSError:
-                    print("Error: EPUB filename appears incorrect.")
-
-print('Download finished.')
+print('\nFinish downloading.')
