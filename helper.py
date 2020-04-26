4cf581cfc0a760d28aa4f0a1a15f90da2907b934
Andrew Mello <mello.a@husky.neu.edu>
2020-04-26 18:38:42 -0400

    Tested implementation

--- helper.py
+++ helper.py
@@ -2,28 +2,38 @@ import requests
 import pandas
 import shutil
 import tqdm
-# import xlrd
 import os
 
 GENRE_HEADER = "English Package Name"
 DEFAULT_TABLE_NAME = "Free+English+textbooks.xlsx"
-TABLE_URL = 'https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4'
-REPLACEMENTS = {'/':'-', '\\':'-', ':':'-', '*':'', '>':'', '<':'', '?':'', \
-                '|':'', '"':''}
+TABLE_URL = "https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4"
+MAX_PATH_LENGTH = 145
+REPLACEMENTS = {
+    "/": "-",
+    "\\": "-",
+    ":": "-",
+    "*": "",
+    ">": "",
+    "<": "",
+    "?": "",
+    "|": "",
+    '"': "",
+}
 
-def make_directories(relative_path):
-    path = os.path.join(os.getcwd(), relative_path)
+
+def make_directories(path):
     if not os.path.exists(path):
         os.makedirs(path)
     return path
 
-def get_table(path):
+
+def get_table(path, force=False):
     """
-    Returns pandas book object. Checks for local copy before
+    Returns pandas ndarray object. Checks for local copy before
     downloading remote copy
     """
     filepath = os.path.join(path, DEFAULT_TABLE_NAME)
-    if os.path.exists(filepath):
+    if os.path.exists(filepath) and not force:
         return pandas.read_excel(filepath)
 
     make_directories(path)
@@ -31,7 +41,8 @@ def get_table(path):
     books.to_excel(filepath)
     return books
 
-def get_book_genres(books):
+
+def get_genres(books):
     """
     Return dictionary where the keys are available genres
     and the values are the number of books in that genre
@@ -43,58 +54,91 @@ def get_book_genres(books):
 
     return count
 
-def download_books(books, output_folder, epubs=True, pdfs=True):
-    books = books[['OpenURL', 'Book Title', 'Author', 'Edition', 'Electronic ISBN', 'English Package Name']].values
-    for url, title, author, edition, isbn, genre in tqdm.tqdm(books):
-        new_folder = make_directories(os.path.join(output_folder, genre))
 
+def get_books_in_genres(books, genre):
+    """
+    Return list of books in given genre
+    """
+    output = []
+    for book in books.iterrows():
+        if genre in book[1][GENRE_HEADER]:
+            output.append(book[1]["Book Title"])
+    return output
+
+
+def download_books(
+    books,
+    output_folder,
+    force=False,
+    selected_genre="",
+    selected_title="",
+    pdf=True,
+    epub=True,
+    confirm_download=False,
+    verbose=False,
+):
+    books = books[
+        [
+            "OpenURL",
+            "Book Title",
+            "Author",
+            "Edition",
+            "Electronic ISBN",
+            "English Package Name",
+        ]
+    ].values
+    books = [b for b in books if selected_genre in b[5]]
+    books = [b for b in books if selected_title in b[1]]
+    count = 0
+    for url, title, author, edition, isbn, genre in tqdm.tqdm(books, disable=verbose):
+        count += 1
+        if verbose:
+            print("Downloading '{}' : ({}/{})".format(title, count, len(books)))
+        path = make_directories(os.path.join(output_folder, genre))
         request = requests.get(url)
         if request.status_code != 200:
-            print("ERROR: Book not found! ({})".format(title))
+            print("ERROR: Book not found! ({}) : {}".format(title, request.status_code))
             continue
 
-        pdf_url = request.url.replace('%2F','/')
-        pdf_url = pdf_url.replace('/book/','/content/pdf/')
-        pdf_url += '.pdf'
         bookname = compose_bookname(title, author, edition, isbn)
-        output_file = os.path.join(new_folder, bookname + '.pdf')
-        _download_book(pdf_url, output_file)
+        if pdf:
+            pdf_url = request.url.replace("%2F", "/")
+            pdf_url = pdf_url.replace("/book/", "/content/pdf/")
+            pdf_url += ".pdf"
+            filepath = os.path.join(path, bookname + ".pdf")
+            _download_book(pdf_url, filepath, force=force)
 
-        epub_url = request.url.replace('%2F','/')
-        epub_url = epub_url.replace('/book/','/download/epub/')
-        epub_url += '.epub'
-        output_file = os.path.join(new_folder, bookname + '.epub')
-        request = requests.get(epub_url, stream=True)
-        if request.status_code != 200:
-            print("WARN: EPUB not found! ({})".format(title))
-            continue
-        
-        _download_book(epub_url, output_file)
+        if epub:
+            epub_url = request.url.replace("%2F", "/")
+            epub_url = epub_url.replace("/book/", "/download/epub/")
+            epub_url += ".epub"
+            filepath = os.path.join(path, bookname + ".epub")
+            _download_book(epub_url, filepath, force=force)
 
-def _download_book(url, bookpath):
-    if not os.path.exists(bookpath):
-        with requests.get(url, stream = True) as req:
-            path = make_directories('tmp')
-            tmp_file = os.path.join(path, '_-_temp_file_-_.bak')
-            with open(tmp_file, 'wb') as out_file:
+
+def _download_book(url, bookpath, force=False):
+    if not os.path.exists(bookpath) or force:
+        with requests.get(url, stream=True) as req:
+            path = make_directories("tmp")
+            tmp_file = os.path.join(path, "_-_temp_file_-_.bak")
+            with open(tmp_file, "wb") as out_file:
                 shutil.copyfileobj(req.raw, out_file)
                 out_file.close()
             shutil.move(tmp_file, bookpath)
 
 
-
-
-
 def compose_bookname(title, author, edition, isbn):
-    bookname = title + ' - ' + author + ', ' + edition + ' - ' + isbn
-    if(len(bookname) > 145):
-        bookname = title + ' - ' + author.split(',')[0] + ' et al., ' + \
-                    edition + ' - ' + isbn
-    if(len(bookname) > 145):
-        bookname = title + ' - ' + author.split(',')[0] + ' et al. - ' + isbn
-    if(len(bookname) > 145):
-        bookname = title + ' - ' + isbn
-    if(len(bookname) > 145):
-        bookname = title[:130] + ' - ' +isbn
-    bookname = bookname.encode('ascii', 'ignore').decode('ascii')
+    bookname = "{} - {}, {} - {}".format(title, author, edition, isbn)
+    if len(bookname) > MAX_PATH_LENGTH:
+        bookname = "{} - {} et al., {} - {}".format(
+            title, author.split(",")[0], edition, isbn
+        )
+    if len(bookname) > MAX_PATH_LENGTH:
+        bookname = "{} - {} et al., {}".format(title, author.split(",")[0], isbn)
+    if len(bookname) > MAX_PATH_LENGTH:
+        bookname = "{} - {}".format(title, isbn)
+    if len(bookname) > MAX_PATH_LENGTH:
+        title_size = MAX_PATH_LENGTH - len(isbn)
+        bookname = "{} - {}".format(title[:title_size], isbn)
+    bookname = bookname.encode("ascii", "ignore").decode("ascii")
     return "".join([REPLACEMENTS.get(c, c) for c in bookname])
--- main.py
+++ main.py
@@ -1,43 +1,87 @@
 #!/usr/bin/env python
 
 
-from tqdm import tqdm
-import requests
 import argparse
 import sys
-import os
 
 import helper
-import gui
 
-def main(output_path, gui=False, list_genres=False):
-    books = helper.get_table(output_path)
-    if gui:
-        app = gui.create()
-        app.populate_genres(helper.get_book_genres(books))
-        app.mainloop()
 
-    if list_genres:
+def main(args):
+    books = helper.get_table(args.output_folder, force=args.force)
+
+    if args.list_genres:
         print("\nAvailable genre options:")
-        genres = helper.get_book_genres(books)
+        genres = helper.get_genres(books)
         for key in sorted(genres):
             print("  '{}': {} books".format(key, genres[key]))
         print()
         return 0
 
-    helper.download_books(books, output_path)
+    if args.list_books is not None:
+        print("\nList of available books:")
+        count = 0
+        books = helper.get_books_in_genres(books, args.list_books)
+        for book in sorted(books):
+            count += 1
+            print("  {}. {}".format(count, book))
+        print()
+        return 0
 
-    print('\nFinish downloading.')
+    if not args.genre and not args.title and not args.all:
+        print("\nNo books selected to download!")
+        print("  Please select books by title (--title) or genre (--genre)")
+        print("  If you want to download everything use --all\n")
+        return 1
+
+    helper.download_books(
+        books,
+        args.output_folder,
+        force=args.force,
+        selected_genre=args.genre,
+        selected_title=args.title,
+        pdf=(not args.only_epub),
+        epub=(not args.only_pdf),
+        confirm_download=args.confirm_before_download,
+        verbose=args.verbose,
+    )
+
+    print("\nFinish downloading.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("output_folder", help="Folder to put downloaded books in")
-    parser.add_argument("--all", help="Download all available book (both PDFs and EPUBs)")
+    parser.add_argument(
+        "--all", help="Download all available book (both PDFs and EPUBs)"
+    )
     parser.add_argument("--only_pdf", help="Downloads only PDFs")
     parser.add_argument("--only_epub", help="Downloads only EPUBs)")
-    parser.add_argument("--list_genres", action='store_true', help="Lists out available genres")
-    parser.add_argument("--only_genres", help="Downloads only books from certain genres")
-    parser.add_argument("--confirm_before_download", help="Prompts user whether to download for each book")
+    parser.add_argument(
+        "--list_genres", action="store_true", help="Lists out available genres"
+    )
+    parser.add_argument(
+        "--list_books",
+        nargs="?",
+        const="",
+        help="Lists available books (add argument to show book by specified genre",
+    )
+    parser.add_argument(
+        "--force", action="store_true", help="Force download even if file exists"
+    )
+    parser.add_argument(
+        "--genre", default="", help="Downloads only books from certain genre"
+    )
+    parser.add_argument(
+        "--title", default="", help="Downloads only books contain argument in title"
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Enables verbose mode"
+    )
+    parser.add_argument(
+        "--confirm_before_download",
+        help="Prompts user whether to download for each book",
+    )
 
     args = parser.parse_args()
-    sys.exit(main(args.output_folder, list_genres=args.list_genres))
+    sys.exit(main(args))
